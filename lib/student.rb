773c1258dbe23dfe0bef1a5160b1dd49ce47d46e
Nicolas Marini <nmarini44@gmail.com>
2018-07-25 22:19:28 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -9,7 +9,7 @@ class Student
   def initialize(student_hash)
 
     student_hash.map do |att, text|
-      self.send(:att = text)
+      self.send(:att=, text)
       binding.pry
     end
     @@all << self
