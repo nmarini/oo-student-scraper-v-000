3471200a9cb735b1c4efe4f2c101aa5d65fd6e53
Nicolas Marini <nmarini44@gmail.com>
2018-07-25 22:00:40 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -7,8 +7,10 @@ class Student
   @@all = []
 
   def initialize(student_hash)
+    student_hash.map do |att|
+      
 binding.pry
-
+  end 
   end
 
   def self.create_from_collection(students_array)
