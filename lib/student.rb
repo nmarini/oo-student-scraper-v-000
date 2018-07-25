0908c77fa6016b7504e93c1ffeb0cdc5834cc30e
Nicolas Marini <nmarini44@gmail.com>
2018-07-25 21:19:16 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,27 +1,26 @@
 class Student
 
-  attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url 
+  attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url
 
   @@all = []
 
   def initialize(student_hash)
-    
+
   end
 
   def self.create_from_collection(students_array)
-    students_array.collect do |student| 
+    students_array.collect do |student|
       student = Student.new
-      @@all << student 
-    end 
-    
+      @@all << student
+    end
+
   end
 
   def add_student_attributes(attributes_hash)
-    
+
   end
 
   def self.all
-    
+
   end
 end
-
