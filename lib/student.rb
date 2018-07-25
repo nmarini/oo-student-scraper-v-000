4991331a8ee44ad4d029099d4279901a09a53e79
Nicolas Marini <nmarini44@gmail.com>
2018-07-25 21:22:34 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -10,7 +10,7 @@ class Student
 
   def self.create_from_collection(students_array)
     students_array.collect do |student|
-      student = Student.new
+      student = Student.new(students_array)
       @@all << student
     end
 
